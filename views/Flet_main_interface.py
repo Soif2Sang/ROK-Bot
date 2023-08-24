1e54a478a7cdd00e9a15472e4d193612dcb6de03
soif2sang <sirefight.gaming@gmail.com>
2023-08-24 15:10:59 +0200

    app routes moved to main_2.py

--- views/Flet_main_interface.py
+++ views/Flet_main_interface.py
@@ -28,40 +28,22 @@ def Main(page: ft.Page, days=950):
     page.tile_manager.refresh()
     page.tile_manager.update_tiles()
 
-    # page.app_routes = [path(
-    #             url="/",
-    #             clear=True,
-    #             view=index
-    #         )]
-    #
-    # page.routing = Routing(
-    #     page=page,  # Here you have to pass the page. Which will be found as a parameter in all your views
-    #     app_routes=page.app_routes,
-    #     # Here a list has to be passed in which we have defined app routing like app_routes
-    # )
 
 def Main_cod(page: ft.Page, days=950):
     page.title = f"Cod Bot - {days} Days left"
     page.frames = {}
     page.window_width = 400
     page.tile_manager = Flet_TileManager_cod.TileManager(page)
+
+    theme = ft.Theme()
+    theme.page_transitions.windows = ft.PageTransitionTheme.FADE_UPWARDS
+    page.theme = theme
+    page.update()
+
     page.add(page.tile_manager)
     page.add(ft.Divider())
     page.tile_manager.refresh()
-    page.update()
     page.tile_manager.update_tiles()
-
-    page.app_routes = [path(
-                url="/",
-                clear=True,
-                view=index
-            )]
-
-    page.routing = Routing(
-        page=page,  # Here you have to pass the page. Which will be found as a parameter in all your views
-        app_routes=page.app_routes,
-        # Here a list has to be passed in which we have defined app routing like app_routes
-    )
 
 if __name__ == "__main__":
     ft.app(target=Main, view=ft.FLET_APP)
