10ff71db69619a26c8c1147aaeca44573ab1e247
Soif2Sang <81446024+Soif2Sang@users.noreply.github.com>
2023-10-19 16:04:32 +0200

    Frametime route is better and fixed bg color is fixed

--- views/profile_settings.py
+++ views/profile_settings.py
@@ -1,20 +1,17 @@
 import flet as ft
 import flet_route
+
 from views.frametime import ManagerTimezone
 
-def viewProfileSettings(page: ft.Page, params: flet_route.Params, basket: flet_route.Basket) -> ft.View:
-    page.window_width = 900
-    page.window_height = 500
 
+def viewProfileSettings(page: ft.Page, params: flet_route.Params, basket: flet_route.Basket) -> ft.View:
     def returnHome():
-        page.window_width = 450
-        page.window_height = 700
         page.go("/")
 
     return ft.View(
         f"/profile/{params.instance_index}/{params.profile_index}/settings",
         controls=[
-            ft.Container(bgcolor="#ecf0f1",
+            ft.Container(bgcolor=ft.colors.SURFACE_VARIANT,
                          content=ft.Row(controls=[
                              ft.IconButton(icon=ft.icons.ARROW_BACK, on_click=lambda _: returnHome()),
                              ft.Text(value="Go back")
