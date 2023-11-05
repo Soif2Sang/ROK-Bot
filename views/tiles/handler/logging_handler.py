c88f4e8a19bb1f8bfcc0965a405223c309f4ac72
soif2sang <sirefight.gaming@gmail.com>
2023-11-05 21:34:51 +0100

    enhancements made + styling

--- views/tiles/handler/logging_handler.py
+++ views/tiles/handler/logging_handler.py
@@ -3,13 +3,15 @@ import flet as ft
 from utils.Task_utils import FileSingleton
 
 fileSingleton = FileSingleton()
+
+
 class Logger(ft.ListView):
-    def __init__(self,frame,page,**kwargs):
+    def __init__(self, frame, page, **kwargs):
         super().__init__(**kwargs)
         self.FileSingleton = FileSingleton()
         self.data = self.FileSingleton.get_data()
         if "interface" not in self.data:
-            self.data["interface"] = {'auto_scroll' : True, 'auto_refresh' : True}
+            self.data["interface"] = {'auto_scroll': True, 'auto_refresh': True}
         self.FileSingleton.write_data(self.data)
         self.auto_scroll = self.data["interface"]["auto_scroll"]
         self.limit_logs = self.data["interface"].get("limit_logs", False)
@@ -17,19 +19,16 @@ class Logger(ft.ListView):
         self.parent = frame
         self.initial_page = page
 
-    def add_text(self, texte:str, color=None):
-        if color is None:
-            text = ft.Text(value=texte,weight=ft.FontWeight.W_600)
-        else:
-            text = ft.Text(value=texte, weight=ft.FontWeight.W_600, color=color)
-            
+    def add_text(self, texte: str, color=None):
+        text = ft.Text(value=texte, weight=ft.FontWeight.W_600, color=color)
+
         if self.limit_logs and len(self.controls) > 300:
             self.controls.pop(0)
         self.controls.append(text)
         self.initial_page.update()
 
     def add_divider(self):
-        if len(self.controls) > 300 and self.limit_logs:
+        if self.limit_logs and len(self.controls) > 300 :
             self.controls.pop(0)
         self.controls.append(ft.Divider())
         self.initial_page.update()
@@ -40,20 +39,20 @@ def get_date():
 
 
 class LoggerUpgrade(ft.ListView):
-    def __init__(self,page,**kwargs):
+    def __init__(self, page, **kwargs):
         super().__init__(**kwargs)
         data = fileSingleton.get_data()
         if "interface" not in data:
-            data["interface"] = {'auto_scroll' : True, 'auto_refresh' : True}
+            data["interface"] = {'auto_scroll': True, 'auto_refresh': True}
         fileSingleton.write_data(data)
-        self.auto_scroll= True
-        self.initial_page= page
+        self.auto_scroll = True
+        self.initial_page = page
 
-    def add_text(self, texte:str, color=None):
+    def add_text(self, texte: str, color=None):
         if color is None:
-            text = ft.Text(value=texte,weight=ft.FontWeight.W_600)
+            text = ft.Text(value=texte, weight=ft.FontWeight.W_600)
         else:
             text = ft.Text(value=texte, weight=ft.FontWeight.W_600, color=color)
         self.controls.append(text)
-        if not isinstance(self.initial_page.controls[-1],ft.Divider):
+        if not isinstance(self.initial_page.controls[-1], ft.Divider):
             self.initial_page.update()
