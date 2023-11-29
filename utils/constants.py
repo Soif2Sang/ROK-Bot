cf9c005cb238583aee4c4f1745ee5e21cdb991cd
Soif2Sang <81446024+Soif2Sang@users.noreply.github.com>
2023-11-29 21:53:45 +0100

    KeyAuth variables set in constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,12 @@
 DEBUG = False
 BREZILIAN = False
-VERSION = "2.2.1"
+VERSION = "2.2.2"
 toasts_history = {}
+
+ownerid = "7oofxdj8uH"
+
+global_name = "Rokbd"
+global_secret = "a968396e3fdfff2a2eaf14516fb283b7b7013e19cf392c863c90e0d8c41d9be0"
+
+brezilian_name = "RokbdBR"
+brezilian_secret = "6d15b7ee5e7312238105efd4b648535835dc1ce5f4250fe2dc82910db43147b6"
