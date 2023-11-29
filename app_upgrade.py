cdc1e42cf53d78e3fd08cac0913f4e1db2177f2b
Soif2Sang <81446024+Soif2Sang@users.noreply.github.com>
2023-11-29 10:47:04 +0100

    app upgrade

--- app_upgrade.py
+++ app_upgrade.py
@@ -1,40 +1,33 @@
 # coding=UTF-8
-import hashlib
 import json
 import os
 import subprocess
 import sys
-import threading
 import traceback
-from datetime import datetime
 from time import sleep
 
 import flet as ft
 from flet_route import Routing, path
 
-from utils.auth import selfApi, update_user_info
-from utils.handle_files import main as HandleFiles
+import views.tiles.tile
+from utils.auth import selfApi
+from utils.Components.AnimatedCard import AnimatedCard
+from utils.Components.filescan import generate_filescan
+from utils.Components.maintenance import generate_maintenance
+from utils.constants import BREZILIAN, toasts_history
+from utils.functions import FileSingleton, getchecksum
+from views.login.login import LoginUI
 
-
-def getchecksum():
-    md5_hash = hashlib.md5()
-    try:
-        file = open("".join(sys.argv), "rb")
-    except:
-        file = open("".join(sys.argv[0]), "rb")
-    md5_hash.update(file.read())
-    digest = md5_hash.hexdigest()
-    return digest
-
+from utils.flet_toast.core import Position
+from utils.flet_toast.toasts_flexible import ToastsFlexible
+from utils.singletons import ApiSingleton, EmulatorSingleton, LinkSingleton
+from views.city_layout import viewCityLayout
+from views.config_path import find_file_in_all_drives
+from views.main import Main
+from views.profile_settings import viewProfileSettings
 
 try:
-    from utils.flet_toast.core import Position
-    from utils.flet_toast.toasts_flexible import ToastsFlexible
-    from utils.singletons import ApiSingleton, LinkSingleton
-    from views.city_layout import viewCityLayout
-    from views.config_path import find_file_in_all_drives
-    from views.main import Main
-    from views.profile_settings import viewProfileSettings
+    1
 except Exception as e:
     exc_type, exc_value, exc_traceback = sys.exc_info()
     traceback_list = traceback.format_exception(exc_type, exc_value, exc_traceback)
@@ -50,10 +43,12 @@ except Exception as e:
         page.update()
 
     keyauthapp = selfApi(
-        name="Rokbd",
+        name="Rokbd" if not BREZILIAN else "RokbdBR",
         ownerid="7oofxdj8uH",
-        secret="a968396e3fdfff2a2eaf14516fb283b7b7013e19cf392c863c90e0d8c41d9be0",
-        version="1.0",
+        secret="a968396e3fdfff2a2eaf14516fb283b7b7013e19cf392c863c90e0d8c41d9be0"
+        if not BREZILIAN
+        else "6d15b7ee5e7312238105efd4b648535835dc1ce5f4250fe2dc82910db43147b6",
+        version="2.0",
         hash_to_check=getchecksum(),
     )
 
@@ -62,256 +57,65 @@ except Exception as e:
     ft.app(target=handleError)
     exit()
 
-toasts_history = {}
 fileSingleton = FileSingleton()
-
-HandleFiles()
-
-
-def is_str_valid(username, password):
-    for element in ["#", "$", "&", "|", "\0", "\n", "\r", "'", "'", '"', "\Z"]:
-        if element in username or element in password:
-            return False
-    return True
-
-
-class LoginUI(ft.Column):
-    def __init__(self, page, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.initial_page = page
-        self.fileSingleton = FileSingleton()
-        self.data = self.fileSingleton.get_data()
-        self.init()
-
-    def show_banner(self, e):
-        def close_banner(e):
-            self.initial_page.banner.open = False
-            self.initial_page.update()
-
-        self.initial_page.banner = ft.Banner(
-            bgcolor=ft.colors.AMBER_100,
-            content=ft.Column(
-                controls=[
-                    ft.TextButton(
-                        icon=ft.icons.LINK_OUTLINED,
-                        text="Pay with Stripe",
-                        on_click=lambda _: self.initial_page.launch_url(
-                            "https://buy.stripe.com/dR66oX4ov0qldkQaEF"
-                        ),
-                    ),
-                    ft.TextButton(
-                        icon=ft.icons.LINK_OUTLINED,
-                        text="Pay with Crypto",
-                        on_click=lambda _: self.initial_page.launch_url(
-                            "https://awesomeseller.mysellix.io/pay/7e1e3c-8597df2730-7d6099"
-                        ),
-                    ),
-                ]
-            ),
-            actions=[
-                ft.TextButton("Close", on_click=close_banner),
-            ],
-            content_padding=ft.padding.all(5),
-        )
-
-        self.initial_page.banner.open = True
-        self.initial_page.update()
-
-    def login(self, e):
-        ApiSingleton().setApiKey("4805a29997857b110ef26530c7f39db1")
-        Main(self.initial_page, 9999)
-        return
-        try:
-            self.initial_page.splash = ft.ProgressBar()
-            self.button_login.disabled = True
-            self.initial_page.update()
-
-            username = self.textfield_username.value
-            password = self.textfield_password.value
-
-            if username == "" or password == "":
-                return
-            if not is_str_valid(username, password):
-                self.initial_page.open_banner("Illegal characters..")
-            if self.initial_page.keyauthapp.login(
-                user=username, password=password, page=self.initial_page
-            ):
-                update_user_info(password, username)
-
-                target_date = datetime.utcfromtimestamp(
-                    int(self.initial_page.keyauthapp.user_data.expires)
-                )
-
-                current_date = datetime.utcnow()
-                days_remaining = (target_date - current_date).days
-
-                self.initial_page.splash = None
-                self.button_login.disabled = False
-                ApiSingleton().setApiKey(self.initial_page.keyauthapp.var("2captcha"))
-                Main(self.initial_page, days_remaining)
-
-                self.initial_page.subscription_checker = threading.Thread(
-                    target=self.verify_subscription, args=(username, password)
-                )
-                self.initial_page.subscription_checker.start()
-            else:
-                sleep(5)
-                self.initial_page.splash = None
-                self.initial_page.loginUI.button_login.disabled = False
-                self.initial_page.update()
-        except Exception as e:
-            print(e)
-            self.initial_page.window_close()
-            os.system("taskkill /f /im flet.exe >nul 2>&1")
-            sys.exit()
-
-    def verify_subscription(self, username, password):
-        try:
-            self.initial_page.keyauthapp = selfApi(
-                name="Rokbd",
-                ownerid="7oofxdj8uH",
-                secret="a968396e3fdfff2a2eaf14516fb283b7b7013e19cf392c863c90e0d8c41d9be0",
-                version="1.0",
-                hash_to_check=getchecksum(),
-            )
-
-            if self.initial_page.keyauthapp.login(
-                user=username, password=password, page=self.initial_page
-            ):
-                target_date = datetime.utcfromtimestamp(
-                    int(self.initial_page.keyauthapp.user_data.expires)
-                )
-
-                current_date = datetime.utcnow()
-                days_remaining = (target_date - current_date).days
-
-                self.initial_page.title = f"RokNet - {days_remaining} Days left"
-                self.initial_page.update()
-                sleep(6 * 3600)
-                return self.verify_subscription(username, password)
-            else:
-                for element in self.initial_page.tile_manager.tiles.values():
-                    element.paused = False
-                    element.stopped = True
-                self.initial_page.window_width = 330
-                self.initial_page.window_height = 330
-                self.initial_page.clean()
-                self.initial_page.add(self.initial_page.loginUI)
-                self.initial_page.update()
-        except Exception as e:
-            print(e)
-            self.initial_page.window_close()
-            os.system("taskkill /f /im flet.exe >nul 2>&1")
-            sys.exit()
-
-    def init(self):
-        self.textfield_username = ft.TextField(
-            label="Username",
-            width=300,
-            value=self.data.get("user", {}).get("username", ""),
-        )
-        self.textfield_password = ft.TextField(
-            label="Password",
-            password=True,
-            can_reveal_password=True,
-            width=300,
-            value=self.data.get("user", {}).get("password", ""),
-        )
-        self.button_login = ft.OutlinedButton(text="Login", on_click=self.login)
-        self.subscribe_button = ft.FilledTonalButton(
-            text="Subscribe", on_click=self.show_banner
-        )
-
-        return self.controls.extend(
-            [
-                self.textfield_username,
-                self.textfield_password,
-                ft.Row(
-                    controls=[
-                        ft.Column(controls=[self.button_login], col=4),
-                        ft.Column(controls=[self.subscribe_button], col=6),
-                    ],
-                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                ),
-            ]
-        )
 
 
 def main(page: ft.Page):
-    # return Main(page, 500)
-
-    page.window_width = 330
-    page.window_height = 330
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.window_width = 450
+    page.window_height = 400
     page.FileSingleton = FileSingleton()
-    path_file = page.FileSingleton.get_path()
 
-    cmd = f"{path_file['HD-Player'].replace('Player', 'Adb')} start-server"
-    subprocess.Popen(cmd)
+    ready = False
 
-    if not os.path.exists(path_file["bluestacks"]) or not os.path.exists(
-        path_file["HD-Player"]
-    ):
-        progress_bar = ft.ProgressBar(visible=True)
-        page.add(progress_bar)
-        page.update()
-        if result := find_file_in_all_drives("bluestacks\.conf"):
-            path_file["bluestacks\.conf".split("\\")[0]] = result
-            with open("./path.json", "w", encoding="UTF-8") as f:
-                json.dump(path_file, f, indent=2)
-        if result := find_file_in_all_drives("HD-Player\.exe"):
-            path_file["HD-Player\.exe".split("\\")[0]] = result
-            with open("./path.json", "w", encoding="UTF-8") as f:
-                json.dump(path_file, f, indent=2)
-        progress_bar.visible = False
-        page.update()
-
-    for i in range(5):
+    for i in range(3):
         ready = False
         try:
             page.keyauthapp = selfApi(
-                name="Rokbd",
+                name="Rokbd" if not BREZILIAN else "RokbdBR",
                 ownerid="7oofxdj8uH",
-                secret="a968396e3fdfff2a2eaf14516fb283b7b7013e19cf392c863c90e0d8c41d9be0",
-                version="1.0",
+                secret="a968396e3fdfff2a2eaf14516fb283b7b7013e19cf392c863c90e0d8c41d9be0"
+                if not BREZILIAN
+                else "6d15b7ee5e7312238105efd4b648535835dc1ce5f4250fe2dc82910db43147b6",
+                version="2.0",
                 hash_to_check=getchecksum(),
             )
             ready = True
         except Exception as e:
             print(e)
             page.keyauthapp = None
-            print("Problem in the database loading..Wait a bit please..")
             sleep(5)
         if ready:
             break
+
     if not ready:
-        page.window_close()
+        page.vertical_alignment = ft.MainAxisAlignment.CENTER
+        page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+        page.add(generate_maintenance())
 
-    def close_banner(e):
-        page.banner.open = False
-        page.update()
+        while 1:
+            sleep(1)
 
-    def open_banner(text):
-        page.banner = ft.Banner(
+    def create_banner(text):
+        return ft.Banner(
             bgcolor=ft.colors.AMBER_100,
             leading=ft.Icon(
                 ft.icons.WARNING_AMBER_ROUNDED, color=ft.colors.AMBER, size=40
             ),
             content=ft.Text(value=text),
             actions=[
-                ft.TextButton("Ok", on_click=lambda: page.close_banner()),
+                ft.TextButton("Ok", on_click=lambda _: page.close_banner()),
             ],
             open=True,
         )
-        page.update()
 
-    page.close_banner = close_banner
-    page.open_banner = lambda text: open_banner(text)
-    page.subscription_checker = threading.Thread()
+    page.open_banner = lambda text: page.show_banner(create_banner(text))
     page.loginUI = LoginUI(page)
     page.UPGRADE = True
-    page.body = ft.Container()
+    page.body = ft.Column()
 
-    def generate_toast(title, description, icon=ft.icons.INFO):
+    def generate_toast(title, description, icon=ft.icons.INFO, bgcolor_title="AMBER"):
         ToastsFlexible(
             page=page,
             icon=icon,
@@ -322,23 +126,26 @@ def main(page: ft.Page):
             width=360,
             set_history=toasts_history,
             position=Position.TOP_RIGHT,
-            bgcolor_title="AMBER",
+            bgcolor_title=bgcolor_title,
         )
 
-    page.generate_toast = lambda title, description, icon=ft.icons.INFO: generate_toast(
-        title, description, icon
+    page.generate_toast = lambda title, description, icon=ft.icons.INFO, bgcolor_title="AMBER": generate_toast(
+        title, description, icon, bgcolor_title
     )
 
     page.app_routes = [
         path(url="/", clear=True, view=index),
+        path(url="/login", clear=True, view=login),
+        path(url=f"/emulator-choice", clear=True, view=emulator_choice),
+        path(url=f"/emulator-loading", clear=True, view=loading_files),
         path(
             url=f"/city-layout/:instance_index/:profile_index",
-            clear=False,
+            clear=True,
             view=viewCityLayout,
         ),
         path(
             url=f"/profile/:instance_index/:profile_index/settings",
-            clear=False,
+            clear=True,
             view=viewProfileSettings,
         ),
     ]
@@ -348,12 +155,145 @@ def main(page: ft.Page):
         app_routes=page.app_routes,
     )
 
-    page.add(page.loginUI)
+    page.go("/login")
     page.update()
+
+    if not BREZILIAN:
+        LinkSingleton().setStripeLink(page.keyauthapp.var("stripe"))
+        LinkSingleton().setSellixLink(page.keyauthapp.var("sellix"))
 
 
 def index(page: ft.Page, params, basket):
     return ft.View(route="/", controls=page.body.controls)
+
+
+def loading_files(page: ft.Page, params, basket):
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    return ft.View(
+        route="/emulator-loading",
+        controls=[ft.ProgressRing(visible=True), generate_filescan()],
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+    )
+
+
+def emulator_choice(page: ft.Page, params, basket):
+    page.window_width = 1920 / 2
+    page.window_height = 1080 / 2
+
+    def go_main(e):
+        path_file = FileSingleton().get_path()
+
+        if "bluestacks" in e.control.data:
+            EmulatorSingleton().setEmulator("bluestacks")
+
+            if not os.path.exists(path_file["bluestacks"]) or not os.path.exists(
+                path_file["HD-Player"]
+            ):
+                page.go("/emulator-loading")
+                page.update()
+
+                if result := find_file_in_all_drives("bluestacks\.conf"):
+                    path_file["bluestacks\.conf".split("\\")[0]] = result
+                    with open("./path.json", "w", encoding="UTF-8") as f:
+                        json.dump(path_file, f, indent=2)
+
+                if result := find_file_in_all_drives("HD-Player\.exe"):
+                    path_file["HD-Player\.exe".split("\\")[0]] = result
+                    with open("./path.json", "w", encoding="UTF-8") as f:
+                        json.dump(path_file, f, indent=2)
+
+            cmd = f"{path_file['HD-Player'].replace('Player', 'Adb')} start-server"
+            subprocess.Popen(cmd)
+
+        elif "ld" in e.control.data:
+            EmulatorSingleton().setEmulator("ld")
+
+            if not path_file.get("LD-Console", False) or not os.path.exists(
+                path_file.get("LD-Console", "fzfgrerg")
+            ):
+                page.go("/emulator-loading")
+                page.update()
+
+                if result := find_file_in_all_drives(r"LDPlayer9\\ldconsole\.exe"):
+                    path_file["LD-Console"] = result
+                    with open("./path.json", "w", encoding="UTF-8") as f:
+                        json.dump(path_file, f, indent=2)
+
+            cmd = f"{path_file['LD-Console'].replace('ldconsole', 'adb')} start-server"
+            subprocess.Popen(cmd)
+
+        Main(page)
+
+    return ft.View(
+        route="/emulator-choice",
+        controls=[
+            ft.Stack(
+                controls=[
+                    ft.Container(
+                        image_src=f"rok_wallpaper.webp",
+                        width=1920 / 2,
+                        height=1080 / 2,
+                        image_fit=ft.ImageFit.COVER,
+                    ),
+                    ft.Row(
+                        controls=[
+                            AnimatedCard("bluestacks_logo.png", go_main),
+                            AnimatedCard("ld_logo.png", go_main),
+                        ],
+                        top=100,
+                        left=240,
+                    ),
+                ]
+            )
+        ],
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        padding=0,
+    )
+
+
+def login(page: ft.Page, params, basket):
+    page.window_width = 1920 / 2
+    page.window_height = 1080 / 2
+    page.window_resizable = False
+    page.title = "RokNet"
+
+    return ft.View(
+        route="/login",
+        controls=[
+            ft.Stack(
+                controls=[
+                    ft.Container(
+                        image_src=f"./rok_wallpaper.webp",
+                        width=1920 / 2,
+                        height=1080 / 2,
+                        image_fit=ft.ImageFit.COVER,
+                    ),
+                    ft.Container(
+                        blur=100,
+                        width=400,
+                        height=250,
+                        right=1920 / 4 - 400 / 2,
+                        top=1080 / 4 - 250 / 2 - 15,
+                        content=ft.Container(
+                            content=page.loginUI,
+                            height=160,
+                            width=300,
+                        ),
+                        alignment=ft.Alignment(0, 0),
+                        border_radius=5,
+                        border=ft.border.all(3, ft.colors.GREY_900),
+                    ),
+                ]
+            )
+        ],
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        padding=0,
+    )
 
 
 if __name__ == "__main__":
