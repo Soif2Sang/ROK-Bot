83250022625ca1cb33f19c9c1641d56fe6428bad
Maxence <maxence@Ordinateur-portable-de-Maxence.local>
2024-04-28 19:01:39 +0200

    Feat: Add support for macos to be able to modify the bot with appli sillicon mac

--- tasks/TaskPC.py
+++ tasks/TaskPC.py
@@ -15,9 +15,14 @@ from time import sleep
 import cv2
 import flet as ft
 import pyautogui
-import win32api
-import win32con
-import win32gui
+
+try:
+
+    import win32api
+    import win32con
+    import win32gui
+except:
+    pass
 from numpy import array, ndarray
 from PIL import Image, ImageFile
 from pytesseract import pytesseract
