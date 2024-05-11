cf8641f92af9770456bd3df93dc53e95b3f4b85e
Soif2Sang <81446024+Soif2Sang@users.noreply.github.com>
2024-05-11 12:01:30 +0200

    Upgrade v.

--- utils/constants.py
+++ utils/constants.py
@@ -1,7 +1,7 @@
 from typing import Literal
 
 DEBUG = False
-VERSION_TYPE: Literal["global", "brazilian"] = "global"
+VERSION_TYPE: Literal["global", "brazilian"] = "brazilian"
 VERSION_NUMBER = "3.3.9"
 TOAST_HISTORY = {}
 
